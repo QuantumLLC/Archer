31b9ba361b7cdee2a20883d87e02eedd03a55c73
QuantumLLC <noreply@dicksweat.shop>
2025-08-16 16:33:57 +0200

    Delete archer directory

--- archer/errors.py
+++
@@ -1,3 +0,0 @@
-class ArcherError(Exception): pass
-class TLSFingerprintError(ArcherError): pass
-class ChallengeError(ArcherError): pass
--- archer/utils.py
+++
@@ -1,5 +0,0 @@
-import random
-import string
-
-def random_string(n=8):
-    return ''.join(random.choice(string.ascii_letters) for _ in range(n))
